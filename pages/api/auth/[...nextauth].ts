f06f84b569263e12a2fab1a3f3a882028b0c51de
deddywolley <deddywolley@gmail.com>
2022-02-06 12:54:14 +0800

    added auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import db from '../../../lib/db'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
 	// Configure one or more authentication providers
-	adapter: PrismaAdapter(db),
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import {
+	Button,
 	Container,
 	Divider,
 	Flex,
 	HStack,
+	Spinner,
 	Stat,
 	StatLabel,
 	StatNumber,
@@ -10,14 +12,18 @@ import {
 	VStack,
 } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { BillList } from '../components/BillList'
 import { Layout } from '../components/Layout'
 import { PayBill } from '../components/PayBill'
 import { getTodaysDate, getTodaysMonth } from '../utils'
 
 const Home: NextPage = () => {
+	const { data: session } = useSession()
+	if (session === undefined) return <Loader />
+	if (!session) return <Login />
 	return (
-		<Layout title='NEXT TEMPLATE' bg='gray.50' color='gray.700'>
+		<Layout title='Bill Tracker App' bg='gray.50' color='gray.700'>
 			<Container
 				maxW='container.sm'
 				h={['100vh', '95vh']}
@@ -34,6 +40,9 @@ const Home: NextPage = () => {
 					<Text fontWeight='bold' fontSize='2xl'>
 						Bill Tracker App
 					</Text>
+					<Button variant='outline' onClick={() => signOut()}>
+						Logout
+					</Button>
 				</Flex>
 				<Text fontSize='sm' color='gray.500'>
 					{getTodaysDate()}
@@ -57,6 +66,72 @@ const Home: NextPage = () => {
 					<BillList />
 				</VStack>
 				<PayBill />
+			</Container>
+		</Layout>
+	)
+}
+
+const Login = () => {
+	return (
+		<Layout title='Bill Tracker App' bg='gray.50' color='gray.700'>
+			<Container
+				maxW='container.sm'
+				h={['100vh', '95vh']}
+				overflow='hidden'
+				rounded='md'
+				bg='white'
+				p='4'
+				shadow='sm'
+				pos='relative'
+				display={'flex'}
+				flexDir={'column'}
+			>
+				<Flex
+					justifySelf={'flex-start'}
+					py='2'
+					alignItems='center'
+					justifyContent='space-between'
+				>
+					<Text fontWeight='bold' fontSize='2xl'>
+						Bill Tracker App
+					</Text>
+				</Flex>
+				<Flex flex='1' alignItems={'center'} justifyContent={'center'}>
+					<Button onClick={() => signIn('google')}>Login</Button>
+				</Flex>
+			</Container>
+		</Layout>
+	)
+}
+
+const Loader = () => {
+	return (
+		<Layout title='Bill Tracker App' bg='gray.50' color='gray.700'>
+			<Container
+				maxW='container.sm'
+				h={['100vh', '95vh']}
+				overflow='hidden'
+				rounded='md'
+				bg='white'
+				p='4'
+				shadow='sm'
+				pos='relative'
+				display={'flex'}
+				flexDir={'column'}
+			>
+				<Flex
+					justifySelf={'flex-start'}
+					py='2'
+					alignItems='center'
+					justifyContent='space-between'
+				>
+					<Text fontWeight='bold' fontSize='2xl'>
+						Bill Tracker App
+					</Text>
+				</Flex>
+				<Flex flex='1' alignItems={'center'} justifyContent={'center'}>
+					<Spinner />
+				</Flex>
 			</Container>
 		</Layout>
 	)
